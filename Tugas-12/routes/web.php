90d36ac37036f15177094fcb592f9690e54ae617
laras <krisyanilaras1309@gmail.com>
2024-02-03 11:10:55 +0700

    Tugas 15: CRUD

--- Tugas-12/routes/web.php
+++ Tugas-12/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CastController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,4 +31,22 @@ Route::get('/table', function() {
 Route::get('/data-tables', function() {
     return view('pages.dataTable');
 });
+
+// Tugas 15 CRUD Builer
+
+Route::get('/cast', [CastController::class, 'index']);
+Route::get('/cast/create', [CastController::class, 'create']);
+Route::post('/cast/store', [CastController::class, 'store']);
+Route::get('/cast/{id}', [CastController::class, 'show']);
+Route::get('/cast/{id}/edit', [CastController::class, 'edit']);
+
+Route::put('/cast/{id}', [CastController::class, 'update']);
+Route::delete('/cast/{id}', [CastController::class, 'destroy']);
+
+
+
+
+
+
+
 
